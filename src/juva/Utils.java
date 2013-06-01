c3250e805a50c9c01d0bc544ca9310643f759a25
shonenada <shonenada@gmail.com>
2013-06-01 17:44:42 +0800

    add json encoder and deconder.

--- src/juva/Utils.java
+++ src/juva/Utils.java
@@ -5,9 +5,64 @@ import java.math.*;
 import java.text.*;
 import java.util.*;
 
-public class Utils {
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
-    public static String MD5(String rawInput) throws NoSuchAlgorithmException{
+public class Utils {
+	
+	public static class Json{
+		
+		public static JSONArray StringToJson(String str){
+			JSONArray output = JSONArray.fromObject(str);
+			return output;
+		}
+		
+		public static JSONArray ArrayToJson(Object[] array){
+			JSONArray output = JSONArray.fromObject(array);
+			return output;
+		}
+		
+		public static JSONArray ArrayListToJson(List list){
+			JSONArray output = JSONArray.fromObject(list);
+			return output;
+		}
+		
+		public static JSONObject MapToJson(Map map){
+			JSONObject output = JSONObject.fromObject(map);
+			return output;
+		}
+		
+		public static List JsonToList(String json){
+			JSONObject jsonObject = JSONObject.fromObject(json);
+			List list = (List) JSONArray
+			                   .toCollection(jsonObject.getJSONArray("array"));
+			return list;
+		}
+		
+		public static JSONArray encode(String str){
+			return StringToJson(str);
+		}
+		
+		public static JSONArray encode(Object[] array){
+			return ArrayToJson(array);
+		}
+		
+		public static JSONArray encode(List list){
+			return ArrayListToJson(list);
+		}
+		
+		public static JSONObject encode(Map map){
+			return MapToJson(map);
+		}
+		
+		public static List decode(String json){
+			return JsonToList(json);
+		}
+		
+	};
+
+    public static String MD5(String rawInput)
+            throws NoSuchAlgorithmException{
         MessageDigest md = MessageDigest.getInstance("MD5");
         md.reset();
         md.update(rawInput.getBytes());
